940c3bd6c8565e54effb362e14124f148c37d707
blackaichi <ecasanovascantarero@gmail.com>
2021-07-14 16:47:05 +0200

    First commit

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Hi! 👋</h1>
+    <p>
+      This page is under development, meanwhile you can follow this <a href="https://www.reddit.com/r/Kitten/">link</a>
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default {}
+//export default Vue.extend({})
+</script>
